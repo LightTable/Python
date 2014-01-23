3f980a889a8099b6c196193b4eecfbd5c4488c1f
Chris Granger <ibdknox@gmail.com>
2014-01-23 14:14:13 -0800

    0.0.3
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject com.lighttable/python "0.0.1"
+(defproject com.lighttable/python "0.0.3"
   :description "Python language plugin for Light Table"
   :dependencies [[org.clojure/clojure "1.5.1"]])
