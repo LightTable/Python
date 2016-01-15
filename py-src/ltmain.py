9be4ce1441ea347bdea8dc4dd76cca49d69086e2
Michelle Baert <rocky.road@rocky-shore.net>
2016-01-15 12:51:38 +0100

    Fixing issue #24 -NameError: name 'unicode' is not defined The Python3 way, using unicode strings instead of bytes.

--- py-src/ltmain.py
+++ py-src/ltmain.py
@@ -46,11 +46,21 @@ def asUnicode(s):
   except:
     return str(s)
 
-def ensureUtf(s):
-  if type(s) == unicode:
-    return s.encode('utf8', 'ignore')
+def ensureUtf(s, encoding='utf8'):
+  """Converts input to unicode if necessary.
+
+  If `s` is bytes, it will be decoded using the `encoding` parameters.
+
+  This function is used for preprocessing /source/ and /filename/ arguments
+  to the builtin function `compile`.
+  """
+  # In Python2, str == bytes.
+  # In Python3, bytes remains unchanged, but str means unicode
+  # while unicode is not defined anymore
+  if type(s) == bytes:
+    return s.decode(encoding, 'ignore')
   else:
-    return str(s)
+    return s
 
 def findLoc(body, line, total):
   for i in range(len(body)):
